c00a240803edc98526b88923ec95c76d4ac6e00c
olmelabs <olmelabs@gmail.com>
2018-09-18 09:52:12 +0100

    updated packages

--- client-app/src/components/HeaderControl.js
+++ client-app/src/components/HeaderControl.js
@@ -23,14 +23,13 @@ class HeaderControl extends React.Component {
 
     return(
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 className="my-0 mr-md-auto font-weight-normal"> </h5>
+        <h5 className="my-0 mr-md-auto font-weight-normal"/>
         <nav className="my-2 my-md-0 mr-md-3">
           <Link className="p-2 text-dark" to="/">Home</Link>
           <Link className="p-2 text-dark" to="game">Game</Link>
           <Link className="p-2 text-dark" to="profile">Profile</Link>
           { accountLink }
         </nav>
-        {/* <a className="btn btn-outline-primary" href="#">Sign up</a> */}
       </div>
     );
   }
