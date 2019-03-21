159fe2bd00865593b22fa1e361e30282ec70d721
olmelabs <olmelabs@gmail.com>
2019-03-21 10:34:38 +0000

    updated home page text

--- client-app/src/components/HomePage.js
+++ client-app/src/components/HomePage.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 export class HomePage extends React.Component {
-
   constructor(props, context) {
     super(props, context);
   }
@@ -12,10 +11,13 @@ export class HomePage extends React.Component {
   render() {
     return (
       <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <p className="lead">Welcome to battle-ship game. Enjoy playing in singlemode or with friends.</p>
+        <p className="lead">Welcome to Battleship game.</p>
         <p test-id="homepage-text">
           <Link to="game">
-            <button className="round-button round-button-80" title="Sigleplayer Game">
+            <button
+              className="round-button round-button-80"
+              title="Sigleplayer Game"
+            >
               <i className="fa fa-laptop fa-2x" />
             </button>
           </Link>
@@ -29,6 +31,15 @@ export class HomePage extends React.Component {
               <i className="fa fa-handshake fa-2x" />
             </button>
           </Link>
+        </p>
+        <p className="lead">
+          Battleship is a guessing game. You can find out more in this{" "}
+          <a href="https://en.wikipedia.org/wiki/Battleship_(game)">
+            wikipedia article
+          </a>
+          . On this site you can have a quick game with computer or enjoy
+          session with your friend. This game is intentioanlly kept simple and
+          close to pen-and-paper version so popular in my childhood.
         </p>
       </div>
     );
